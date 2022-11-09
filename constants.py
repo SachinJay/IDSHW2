5e8728a509bc286d21dd35f34af88a3c7d5875ba
SachinJay <sjd232@cornell.edu>
2022-11-09 12:46:49 -0500

    Adding filtering to reduce frequency of alerts to only important events

--- constants.py
+++ constants.py
@@ -6,3 +6,8 @@ SECURITY = "Security"
 EVENT_ATTRS = ["TimeGenerated", "StringInserts"]
 SEC_ATTRS = ["TimeGenerated", "StringInserts"]
 TIME_ATTR = "TimeGenerated"
+STRING_ATRR = "StringInserts"
+
+BLOCKED_PATHS = ["C:\\Users\\sachi\\OneDrive", "C:\\Users\\sachi\\OneDrive\\Desktop"]
+BLOCKED_DIRS = ["\\.git", "\\G++", "\\commons-math", "\\openmrs-core",
+ "\\Desktop\\hw-1"]
--- sec.py
+++ sec.py
@@ -6,11 +6,51 @@ from utils import convert_windows_date
 import win32evtlog
 
 
-from constants import SECURITY, TIME_ATTR
+from constants import BLOCKED_DIRS, BLOCKED_PATHS, SECURITY, STRING_ATRR, TIME_ATTR
 from constants import EVENT_ATTRS
 
 
-def filter_sec(attr, value):
+def is_bad_path(path : str) -> bool:
+    """
+    Returns true if this path makes the even skippable
+    """    
+
+    if path in BLOCKED_PATHS:
+        return True
+
+    for dir in BLOCKED_DIRS:
+        if dir in path:
+            return True
+
+    if "C:\\Users\\sachi\\OneDrive" not in path:
+        return True
+
+    return False
+
+
+def can_skip_event(event) -> bool:
+    """
+    Checks if we can skip an event
+    We can skip an event for a number of reasons
+        - If the event is common: e.g. most file touches will happen in the 
+        C:\\Users directory so events regarding only that dir aren't interesting
+        - If the event is irrelevant: e.g. events not trigerred by a human
+        aren't very interesting. I'm not sure if I should wholesale disregard
+        them though?
+    """
+    value = getattr(event, STRING_ATRR)
+
+    try:
+        path = value[6]
+        if is_bad_path(path):
+            return True
+    except Exception as e:
+        print(f"Value {value} caused {e}")
+
+    return False
+
+
+def clean_sec_value(attr, value):
     # Filters out useless info from StringInserts
     if attr == "StringInserts":
         try:
@@ -41,9 +81,14 @@ def print_sec():
             break
         if events:
             for event in events:
+                if can_skip_event(event):
+                    # If we can skip this event, go to the next event
+                    continue
+
                 for attr in EVENT_ATTRS:
+
                     value = getattr(event, attr)
-                    value = filter_sec(attr, value)
+                    value = clean_sec_value(attr, value)
                     print(value, end = " ")
                 print()
 
