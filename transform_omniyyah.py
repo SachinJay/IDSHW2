fbf38ea3cfd5f6a1ac62049008dc852eb28d402b
SachinJay <sjd232@cornell.edu>
2022-11-27 11:38:16 -0500

    Finished the code to transform Omniyyah's data

--- transform_omniyyah.py
+++ transform_omniyyah.py
@@ -4,14 +4,15 @@ into a format my model can handle
 """
 
 import sys
+import csv
 
 import pandas as pd
 
-from utils import convert_windows_date
+from utils import convert_omniyyah_date_time
 
 DROP_COLS = ["EventCategory", "EventSource", "EventID", "Account", "TESTDATA"]
 
-def transform(input_csv = "test.csv", output_csv = "output.csv"):
+def transform(input_csv = "test.csv", output_csv = "tmp.csv"):
     """
     Transforms Omniyyah's data to something my model can test on
     """
@@ -21,6 +22,7 @@ def transform(input_csv = "test.csv", output_csv = "output.csv"):
     # Remove the unnamed column
     csv_input = csv_input.loc[:, ~csv_input.columns.str.contains("Unnamed")]
 
+    # drop columns I don't use in my sensor
     for col in DROP_COLS:
         # axis = 1 in order to drop a column instead of a row
         csv_input.drop(col, inplace=True, axis=1)
@@ -33,15 +35,63 @@ def transform(input_csv = "test.csv", output_csv = "output.csv"):
         
     # Adds class col
     csv_input['Class'] = ["Normal"] * len(csv_input)
+
+    # Rename ProcessName
+    csv_input.rename(columns={"ProcessName" : "C:\\Users\\sachi\\OneDrive\\Documents\\IDS\\HW2\\IDSHW2"}, inplace=True)
+
+
+    # Write to csv
     csv_input.to_csv(output_csv, index=False)
+
+    
+
+def add_time(input_csv = "tmp.csv", output_csv = "output.csv"):
+    # First, add the two new columns for day and time
+    
+    in_csv = open(input_csv, 'r')
+    out_csv = open(output_csv, 'w')
+
+    writer = csv.writer(out_csv, lineterminator='\n')
+    reader = csv.reader(in_csv)
+
+    all = []
+
+    # Header
+    row = next(reader)
+    row.insert(0, "11")
+    row.insert(0,"4")
+    all.append(row)
+
+    for row in reader:
+        # ASSUMES time generated is the first column
+        time_gen = row[0]
+        day, hour = convert_omniyyah_date_time(time_gen)
+
+        row.insert(0,hour)
+        row.insert(0,day)
+
+        all.append(row)
+    
+    writer.writerows(all)
+
+    in_csv.close()
+    out_csv.close()
+
+    # second, delete the time generated column
+    df = pd.read_csv(output_csv)
+    df.drop("TimeGenerated", inplace=True, axis=1)
+    df.to_csv(output_csv, index=False)
+
 
 def driver():
     argc = len(sys.argv)
 
     if argc == 1:
         transform()
+        add_time()
     elif argc == 3:
-        transform(sys.argv[1], sys.argv[2])
+        transform(input_csv=sys.argv[1])
+        add_time(output_csv=sys.argv[2])
     else:
         print("Need an input and output csv or no args at all")
         exit(1)
--- utils.py
+++ utils.py
@@ -4,10 +4,40 @@ from datetime import date
 date_ex = "2022-11-09 11:36:38"
 
 def convert_windows_date(date_str : str):
+    """
+    Params
+    -------
+    date_str : str
+        the time generated attribute
+    
+    Returns
+    --------
+    day,hour : tuple[int, int]
+        the day and hour of the time generated
+    """
     date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
     day : int = date_obj.weekday()
     hour : int = int(date_obj.strftime("%H"))
 
     return day,hour
 
+def convert_omniyyah_date_time(date_str : str):
+    """
+    Time generated in Omniyyah's files are in a different format somehow
+
+    Params
+    -------
+    date_str : str
+        the time generated attribute
+    
+    Returns
+    --------
+    day,hour : tuple[int, int]
+        the day and hour of the time generated
+    """
+    date_obj = datetime.strptime(date_str, '%m/%d/%Y %H:%M')
+    day : int = date_obj.weekday()
+    hour : int = int(date_obj.strftime("%H"))
+
+    return day,hour
 
